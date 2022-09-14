856b9f6a123d514aa9a1bee6bea5d21227773b99
monabberhossain <mn2world@gmail.com>
2022-09-15 03:33:41 +0600

    start button clicked solved

--- script.js
+++ script.js
@@ -107,13 +107,13 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
+    countdownOverlay.innerHTML = '<h1>${count}</h1>';
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "hidden";
+      countdownOverlay.style.display = "flex";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
